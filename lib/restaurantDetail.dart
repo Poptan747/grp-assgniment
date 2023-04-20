2ab8d1515622c42e1604b1fa446fa8627ca08716
Poptan47 <66237020+Poptan47@users.noreply.github.com>
2023-04-21 02:29:22 +0800

    update app icon & restaurantDetail page

--- lib/restaurantDetail.dart
+++ lib/restaurantDetail.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'dart:math';
 import 'home.dart';
 
+
+List <Map<String,dynamic>> restaurantItem = [
+
+  {'path': 'assets/foodcourt3.jpg', 'restaurantName': "Uncle Wang FoodCourt", 'restaurantCategory': "Food Court" , 'restaurantDesc': "A food court is a large, indoor or outdoor dining area with multiple food stalls or restaurants, offering a variety of cuisines and dishes in a convenient and communal setting.", 'location': 'Location - Taman Sutera', 'rating': 3.0},
+  {'path': 'assets/cafe2.jpg', 'restaurantName': "Journey Dot Com", 'restaurantCategory': "Cafe" , 'restaurantDesc': "A cafe is a charming and inviting eatery that offers a comfortable and laid-back ambiance for customers to enjoy delicious coffee and light meals while socializing, working, or simply relaxing.", 'location': 'Location - Impian Emas', 'rating': 5.0},
+  {'path': 'assets/mcd1.jpg', 'restaurantName': "McDonald's", 'restaurantCategory': "Fast food" , 'restaurantDesc': "McDonald's (MCD) is a fast-food company that operates and franchises a chain of restaurants worldwide, serving burgers, fries, chicken, and other popular fast food items.", 'location': 'Location - Setia Tropika', 'rating': 4.0}
+];
+
+int pos = Random().nextInt(3);
+
+String imagePath = restaurantItem[pos]['path'];
+String restaurantName = restaurantItem[pos]['restaurantName'];
+String restaurantCategory = restaurantItem[pos]['restaurantCategory'];
+String restaurantDesc = restaurantItem[pos]['restaurantDesc'];
+String restaurantLocation = restaurantItem[pos]['location'];
+double restaurantRating = restaurantItem[pos]['rating'];
+
+Icon chgIcon(){
+  switch (restaurantCategory) {
+    case 'Fast food':
+      return Icon(Icons.fastfood,size: 15,color: Colors.white);
+    case 'Cafe':
+      return Icon(Icons.coffee,size: 15,color: Colors.white);
+    case 'Food Court':
+      return Icon(Icons.storefront,size: 15,color: Colors.white);
+    default :
+      return Icon(Icons.restaurant,size: 15,color: Colors.white);
+  }
+}
 class RestaurantDetailPage extends StatelessWidget {
   const RestaurantDetailPage({Key? key}) : super(key: key);
 
@@ -23,7 +53,7 @@ class RestaurantDetailPage extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('mcd1.jpg'),
+            image: AssetImage(imagePath),
             fit: BoxFit.cover
           )
         ),
@@ -51,7 +81,7 @@ class RestaurantDetailPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("McDonald's",style: TextStyle(color: Colors.white, fontSize: 30, height: 1.2, fontWeight: FontWeight.bold)
+                      Text(restaurantName,style: TextStyle(color: Colors.white, fontSize: 30, height: 1.2, fontWeight: FontWeight.bold)
                       ),
                       SizedBox(
                         height: 20,
@@ -62,13 +92,13 @@ class RestaurantDetailPage extends StatelessWidget {
                             GFButton(
                               onPressed: (){},
                               color: Colors.black,
-                              text: "Fast food",
+                              text: restaurantCategory,
                               shape: GFButtonShape.pills,
-                              icon: Icon(Icons.fastfood,size: 15,color: Colors.white),
+                              icon: chgIcon(),
                             ),
                             SizedBox(width: 10),
                             GFRating(
-                              value: 3.5,
+                              value: restaurantRating,
                               size: 20,
                               color: Colors.yellow,
                               borderColor: Colors.white,
@@ -81,13 +111,13 @@ class RestaurantDetailPage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 5),
-                      Text("McDonald's (MCD) is a fast-food company that operates and franchises a chain of restaurants worldwide, serving burgers, fries, chicken, and other popular fast food items.", style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15),),
+                      Text(restaurantDesc, style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15),),
                       GFButton(
                           onPressed: (){},
                           shape: GFButtonShape.pills,
                           color: Colors.black,
                           icon: Icon(Icons.location_on,size: 15,color: Colors.white),
-                          text: 'Location - Setia Tropika', textColor: Colors.white,
+                          text: restaurantLocation, textColor: Colors.white,
                       ),
                     ], //'Location - Setia Tropika',style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15
                   ),
